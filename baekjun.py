06723c60d8a705df23ce81f756cbe126907e08a8
김문영 <wgansdud2@naver.com>
2023-11-18 16:23:58 +0900

    백준 10845 큐

--- baekjun.py
+++ baekjun.py
@@ -1,55 +1,38 @@
-# import sys
-
-# sens = list(sys.stdin.readline())
-# sens = sens[:-1]
-# cur = len(sens)
-# ran = int(input())
-
-# for i in range(ran):
-#     inpu = input().split(' ')
-#     if inpu[0] == "L":
-#         if cur != 0:
-#             cur -= 1
-#     elif inpu[0] == "D":
-#         if cur != len(sens):
-#             cur += 1
-#     elif inpu[0] == "B":
-#         if cur != 0:
-#          del sens[cur-1]
-#          cur -= 1
-
-#     elif inpu[0] == "P":
-#         sens.insert(cur,inpu[1])
-#         cur += 1
-
-            
-# print(''.join(sens)) //시간초과
-
-
-# 접근법은, 커서를 기준으로 문자열을 스택 두개에 나누어 담겠다는 것이다
-#
 
 import sys
 
-left = list(sys.stdin.readline().rstrip())
-right = []
-ran = int(input())
+# left = list(sys.stdin.readline().rstrip())
+# right = []
+ans = []
+ran = int(sys.stdin.readline())
 
 for i in range(ran):
-    inpu = list(sys.stdin.readline().split())
-    if inpu[0] == "L":
-        if len(left) != 0:
-            right.append(left.pop())
-            
-    elif inpu[0] == "D":
-        if len(right)!= 0:
-            left.append(right.pop())
-    elif inpu[0] == "B":
-        if len(left) != 0:
-            left.pop()
+    com =  list(map(str, sys.stdin.readline().split()))
+    if com[0] == "push":
+        ans.append(com[1])
+    elif com[0]=="pop":
+        if len(ans):
+            print(ans[0])
+            del ans[0]
+        else:
+            print(-1)
+    elif com[0]=="size":
+        print(len(ans))
+    elif com[0]=="front":
+        if len(ans):    
+             print(ans[0])
+        else:
+            print(-1)  
+    elif com[0]=="back":
+        if len(ans):
+            print(ans[-1])
+        else:
+            print(-1)
+    else:
+        if len(ans):
+            print(0)
+        else:
+            print(1)
+        
 
-    elif inpu[0] == "P":
-        left.append(inpu[1])
 
-            
-print(''.join(left)+''.join(reversed(right)))
